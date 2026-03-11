bccf3e64fb86be2cbcc3288ad9cd8d2554e7d460
FernandaJaco <jacovaleria9@gmail.com>
2026-03-11 08:23:31 -0600

    nuuu

--- practica1132026/Program.cs
+++ practica1132026/Program.cs
@@ -11,51 +11,53 @@ class EjemploClase
         string contrasena = "javier";
         string alumno = "2025GJ601";
         string contraseñaAlumno = "1234";
-       
-        Console.WriteLine("Usuario:");
-        usuario = Console.ReadLine();
-        if (usuario==user)
+        intentos = 0;
+
+       while (intentos < 3)
         {
+            Console.WriteLine("Usuario:");
+            usuario = Console.ReadLine();
             Console.WriteLine("Contraseña:");
             password = Console.ReadLine();
-            if (password == contrasena)
-            {
-                Console.WriteLine("Acceso correcto");
+            if (usuario == user && password==contrasena) 
+            { 
+                    Console.WriteLine("Acceso correcto");
                 Console.WriteLine("Perfil de docente");
-                Console.WriteLine("1. Notas");
-                Console.WriteLine("2. Salir");
-                Console.WriteLine("Ingresar una opción");
-                op = int.Parse(Console.ReadLine());
-                if (op == 1)
-                {
-                    Console.WriteLine("Ingresar notas del período 1");
-                    Console.WriteLine("Ingresar la nota de Actividad 1:");
-                    nota1 = double.Parse(Console.ReadLine());
-                    Console.WriteLine("Ingresar la nota de Actividad 2:");
-                    nota2 = double.Parse(Console.ReadLine());
-                    Console.WriteLine("Ingresar la nota del Parcial:");
-                    nota3 = double.Parse(Console.ReadLine());
-                    Console.WriteLine("Nota Primer Período");
-                    promedio = (nota1 * .25) + (nota2 * .25) + (nota3 * .5);
-                    Console.WriteLine(promedio);
+                    Console.WriteLine("1. Notas");
+                    Console.WriteLine("2. Salir");
+                    Console.WriteLine("Ingresar una opción");
+                    op = int.Parse(Console.ReadLine());
+                    if (op == 1)
+                    {
+                        Console.WriteLine("Ingresar notas del período 1");
+                        Console.WriteLine("Ingresar la nota de Actividad 1:");
+                        nota1 = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingresar la nota de Actividad 2:");
+                        nota2 = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingresar la nota del Parcial:");
+                        nota3 = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Nota Primer Período");
+                        promedio = (nota1 * .25) + (nota2 * .25) + (nota3 * .5);
+                        Console.WriteLine(promedio);
 
-                }
-                else
-                {
-                    Console.WriteLine("Saliendo...");
-                }
-                }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Saliendo..."); break;
+                    }
+               
+            }
             else
             {
-                Console.WriteLine("Contraseña incorrecta");
+                Console.WriteLine("Usuario y contraseña incorrecta");
             }
+            intentos++;
         }
-        else
+      
+        if (intentos == 3)
         {
-            Console.WriteLine("Usuario incorrecto");
+            Console.WriteLine("Cuenta bloqueada");
         }
-
-
     }
-
-    }
+}
+        

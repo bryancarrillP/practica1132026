0ba04b9bf0883a9d59328e754845013f69ea23f3
FernandaJaco <jacovaleria9@gmail.com>
2026-03-11 08:04:56 -0600

    Perfil Docente

--- practica1132026/Program.cs
+++ practica1132026/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Mail;
+
+class EjemploClase
+{
+    public static void Main(string[] args)
+    {
+        double nota1, nota2, nota3, promedio;
+        string usuario, password, usuario_alu, contraalumno;
+        int op, intentos;
+        string user = "javier.garcia@catolica.edu.sv";
+        string contrasena = "javier";
+        string alumno = "2025GJ601";
+        string contraseñaAlumno = "1234";
+       
+        Console.WriteLine("Usuario:");
+        usuario = Console.ReadLine();
+        if (usuario==user)
+        {
+            Console.WriteLine("Contraseña:");
+            password = Console.ReadLine();
+            if (password == contrasena)
+            {
+                Console.WriteLine("Acceso correcto");
+                Console.WriteLine("Perfil de docente");
+                Console.WriteLine("1. Notas");
+                Console.WriteLine("2. Salir");
+                Console.WriteLine("Ingresar una opción");
+                op = int.Parse(Console.ReadLine());
+                if (op == 1)
+                {
+                    Console.WriteLine("Ingresar notas del período 1");
+                    Console.WriteLine("Ingresar la nota de Actividad 1:");
+                    nota1 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingresar la nota de Actividad 2:");
+                    nota2 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingresar la nota del Parcial:");
+                    nota3 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Nota Primer Período");
+                    promedio = (nota1 * .25) + (nota2 * .25) + (nota3 * .5);
+                    Console.WriteLine(promedio);
+
+                }
+                else
+                {
+                    Console.WriteLine("Saliendo...");
+                }
+                }
+            else
+            {
+                Console.WriteLine("Contraseña incorrecta");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Usuario incorrecto");
+        }
+
+
+    }
+
+    }
